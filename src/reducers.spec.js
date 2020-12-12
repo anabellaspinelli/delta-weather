92cde622acb14f5832c2aad1a9f03fcd773cdf10
Anabella Spinelli <anabella.spinelli@typeform.com>
2020-12-12 09:51:03 -0300

    test: temperature reducer

---
+++ src/reducers.spec.js
@@ -0,0 +1,66 @@
+// {
+//     status: 'idle',
+//     temperatures: null,
+//     locationName: null,
+//     error: null,
+// }
+
+import { initialState, temperatureReducer } from './reducers'
+
+describe('temperature reducer', () => {
+    test('action type: error', () => {
+        const prevState = { ...initialState }
+        const theError = new Error('an error')
+        const action = {
+            type: 'error',
+            error: theError,
+        }
+        const expectedState = {
+            locationName: null,
+            temperatures: null,
+            status: 'errored',
+            error: theError,
+        }
+
+        expect(temperatureReducer(prevState, action)).toEqual(expectedState)
+    })
+
+    test('action type: success', () => {
+        const prevState = { ...initialState }
+        const action = {
+            type: 'success',
+            locationName: 'Buenos Aires, Argentina',
+            temperatures: {
+                today: 24,
+                yesterday: 22,
+            },
+        }
+        const expectedState = {
+            status: 'resolved',
+            locationName: 'Buenos Aires, Argentina',
+            temperatures: {
+                today: 24,
+                yesterday: 22,
+            },
+            error: null,
+        }
+
+        expect(temperatureReducer(prevState, action)).toEqual(expectedState)
+    })
+
+    test('action type: started', () => {
+        const prevState = { ...initialState }
+        const action = {
+            type: 'started',
+            locationName: 'Buenos Aires, Argentina',
+        }
+        const expectedState = {
+            status: 'pending',
+            locationName: null,
+            temperatures: null,
+            error: null,
+        }
+
+        expect(temperatureReducer(prevState, action)).toEqual(expectedState)
+    })
+})
