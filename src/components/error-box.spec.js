7212ad03ec6c2682e79fc5012f94265bc602d5a6
Anabella Spinelli <anabella.spinelli@typeform.com>
2020-12-12 10:02:37 -0300

    test: error box snapshot

---
+++ src/components/error-box.spec.js
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { ErrorBox } from './error-box'
+
+test('loads and displays greeting', async () => {
+    const errorBoxProps = {
+        errorMessage: 'Ooops testing error',
+    }
+
+    const { container } = render(<ErrorBox {...errorBoxProps} />)
+
+    expect(container).toMatchInlineSnapshot(`
+        <div>
+          <div
+            class="fade alert alert-danger show"
+            role="alert"
+            style="margin-top: 16px;"
+          >
+            <div
+              class="alert-heading h4"
+            >
+              Oh no, thats bad
+            </div>
+            <p>
+              There was an error fetching the weather, read below for details:
+            </p>
+            <pre
+              style="white-space: pre-wrap;"
+            >
+              Ooops testing error
+            </pre>
+          </div>
+        </div>
+    `)
+})
