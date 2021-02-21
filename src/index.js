c3db77608eac6c3a120b3e3477a893d4670e8b1c
Anabella Spinelli <anabellaspinelli@gmail.com>
2021-02-21 12:36:41 +0100

    add mock service worker lib

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./mocks/browser')
+    worker.start()
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
